da6e0aad8e160fdd50856ff7aa6598f66d0eadda
htmloiiai <canshark1130@gmail.com>
2025-08-06 22:06:47 +0800

    upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll(".button1, .button2, .button3, .button4").forEach(btn => {
+document.querySelectorAll(".button1, .button2, .button3, .button4, .button5").forEach(btn => {
     btn.addEventListener("click", () => {
         const url = btn.getAttribute("data-url")
         window.location.href = url;
